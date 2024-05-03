3b46a6e579a03ed82a8ef3af415081f62e4291bb
Haidar Jaded <haidar.king787@gmail.com>
2024-05-03 14:18:08 +0300

    Imrovments on dashbard for performence

--- components/DashboardComponent/Cards/Cards.jsx
+++ components/DashboardComponent/Cards/Cards.jsx
@@ -1,46 +1,38 @@
-import MailIcon from "@mui/icons-material/Mail";
 import { useEffect, useState } from 'react';
-import { device } from "../../../Routes";
 import { OutlinedCard } from "./Card";
-import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import TvIcon from '@mui/icons-material/Tv';
 import DoneIcon from '@mui/icons-material/Done';
 import BuildIcon from '@mui/icons-material/Build';
 import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
 import PieChartRoundedIcon from '@mui/icons-material/PieChartRounded';
 import HourglassDisabledRoundedIcon from '@mui/icons-material/HourglassDisabledRounded';
-import { completedDevices } from "../../../Routes/api/completedDevices";
+import axiosInstance from "../../../utils/auth/axiosInstance"
+import { responseErrorHandlers } from "../../../wrappers";
+import getConfig from "next/config";
 
 export function Cards() {
-    const [devices, setDevices] = useState([]);
+    const [dashbardInfo, setDashbardInfo] = useState({});
     useEffect(() => {
-        const getDevices = async () => {
-            const params = {
-                'repaired_in_center': 1,
-                'with': 'client,user',
-                'deliver_to_client': 0
+        const getDashboardInfo = async () => {
+            try {
+                const { publicRuntimeConfig } = getConfig();
+                const DASHBOARD_INFO_URL = "api/dashboard_info";
+                const BASE_URL = `${publicRuntimeConfig.apiUrl}`;
+
+                const responseData = await axiosInstance
+                    .get(`${BASE_URL}${DASHBOARD_INFO_URL}`)
+                    .then(async (response) => {
+                        return response?.data?.body;
+                    });
+
+                setDashbardInfo(responseData);
+            } catch (error) {
+                responseErrorHandlers(error?.response);
             }
-            const data = await device.getAll(params);
-            // setDevices(data)
-            data?setDevices(data):setDevices([]);
         };
-        getDevices();
+        getDashboardInfo();
     }, [])
 
-    const [completed_devices, setCompletedDevices] = useState([]);
-    useEffect(() => {
-        const getDevices = async () => {
-            const params = {
-                'repaired_in_center': 1,
-            }
-            const data = await completedDevices.getAll(params);
-            // setCompletedDevices(data)
-            data?setCompletedDevices(data):setCompletedDevices([]);
-        };
-        getDevices();
-    }, [])
-    const currentDate = new Date();
-    const currentMonth = currentDate.getMonth() + 1;
     return (
         <>
             <div style={{
@@ -52,32 +44,26 @@ export function Cards() {
                 justifyContent: ' flex-start'
 
             }}>
-                <OutlinedCard name='عدد الأجهزة الموجودة في المركز' number={devices.length}>
+                <OutlinedCard name='عدد الأجهزة الموجودة في المركز' number={dashbardInfo.devices_count}>
                     <TvIcon />
                 </OutlinedCard>
 
-                <OutlinedCard name='عدد الجاهز منها' number={devices.filter(device => device.status === 'جاهز').length}>
+                <OutlinedCard name='عدد الجاهز منها' number={dashbardInfo.ready_devices_count}>
                     <DoneIcon />
                 </OutlinedCard>
-                <OutlinedCard name='عدد الذي يتم العمل عليه' number={devices.filter(device => device.status === 'قيد العمل').length}>
+                <OutlinedCard name='عدد الذي يتم العمل عليه' number={dashbardInfo.in_progress_devices_count}>
                     <BuildIcon />
                 </OutlinedCard>
-                <OutlinedCard name='عدد الاجهزة القابلة للتسليم' number={devices.filter(device => device.status === 'لا يصلح'
-                    || device.status === 'لم يوافق على العمل به' || device.status === 'غير جاهز' || device.status === 'جاهز').length}>
+                <OutlinedCard name='عدد الاجهزة القابلة للتسليم' number={dashbardInfo.deliverable_devices_count}>
                     <BuildIcon />
                 </OutlinedCard>
-                <OutlinedCard name=' عدد التي تم تسليمها جاهزة' number={completed_devices.filter(completed_device => completed_device.status === 'جاهز').length}>
+                <OutlinedCard name=' عدد التي تم تسليمها جاهزة' number={dashbardInfo.ready_completed_devices_count}>
                     <LocalShippingOutlinedIcon />
                 </OutlinedCard>
-                <OutlinedCard name=' عدد التي تم تسليمها غير جاهزة' number={completed_devices.filter(completed_device => completed_device.status === 'غير جاهز'
-                    || completed_device.status === 'لا يصلح').length}>
+                <OutlinedCard name=' عدد التي تم تسليمها غير جاهزة' number={dashbardInfo.unready_completed_devices_count}>
                     <PieChartRoundedIcon />
                 </OutlinedCard>
-                <OutlinedCard name='عدد الأجهزة التي تم تسليمها هذا الشهر' number={completed_devices.filter(completed_device => {
-                    const deliveryDate = new Date(completed_device.date_delivery);
-                    const deliveryMonth = deliveryDate.getMonth() + 1;
-                    return deliveryDate.getFullYear() === currentDate.getFullYear() && deliveryMonth === currentMonth;
-                }).length}>
+                <OutlinedCard name='عدد الأجهزة التي تم تسليمها هذا الشهر' number={dashbardInfo.completed_devices_count_in_this_month}>
                     <HourglassDisabledRoundedIcon />
                 </OutlinedCard>
             </div>
